86ae34aac38b3d7070fbe0905226f688855a9739
d-kens <omondikens1@gmail.com>
2023-10-17 05:49:11 +0300

    working with arrays

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        System.out.println(Arrays.toString(numbers));
+
+        // cleaner way - when you know the array elements ahead of time
+        int[] values = {40, 50, 60, 70, 80, 90, 100};
+        System.out.println(Arrays.toString(values));
+    }
+}
