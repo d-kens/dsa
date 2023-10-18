80dc6ef6daf85977ec36a4b867e233bc4c8511f3
d-kens <omondikens1@gmail.com>
2023-10-18 05:50:19 +0300

    array class - insert items, print items

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,14 @@ public class Main {
         // cleaner way - when you know the array elements ahead of time
         int[] values = {40, 50, 60, 70, 80, 90, 100};
         System.out.println(Arrays.toString(values));
+
+
+        Array test = new Array(3);
+        test.insert(10);
+        test.insert(20);
+        test.insert(30);
+        test.insert(70);
+        test.insert(100);
+        test.print();
     }
 }
